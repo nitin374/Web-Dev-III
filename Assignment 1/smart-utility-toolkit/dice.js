7bccf5954ea88fcdeb6d4b4b118056b35fcfc2b0
Nitin <nitin10296+a@gmail.com>
2026-08-31 23:16:50 +0530

    Delete Assignment 1/smart-utility-toolkit directory

--- Assignment 1/smart-utility-toolkit/dice.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-const numberOfRolls = 5;
-
-for (let roll = 1; roll <= numberOfRolls; roll++) {
-  const diceValue = crypto.randomInt(1, 7);
-  console.log(`Dice Rolled: ${diceValue}`);
-}
